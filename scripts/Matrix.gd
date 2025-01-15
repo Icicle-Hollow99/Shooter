347229e7d6f1e8867e14bfc4c87bddc75afdda8e
Icicle-Hollow <ishan_mahapatra@oakridge.in>
2025-01-15 13:14:04 +0530

    #rd Commit

--- scripts/Matrix.gd
+++ scripts/Matrix.gd
@@ -11,4 +11,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var direction : Vector2 = Vector2.ZERO
+	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
+	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
+	
+	velocity = direction * move_speed
+	
+	
 	pass
+
+func _physics_process( delta ):
+	move_and_slide()
